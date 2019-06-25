d7c803f367e417ab44cc3432ad2d0019fb4ccd1f
Oskar Risberg <oskar.risberg@stendahls.se>
2019-06-25 17:16:28 +0200

    Change to a package actually updated in the last 5 years

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var async = require('async');
 var plist = require('simple-plist');
 var decompress = require('decompress-zip');
-var provisioning = require('provisioning');
+var provisioning = require('@stendahls/provision-parse');
 var entitlements = require('entitlements');
 
 var rimraf = require('rimraf');
